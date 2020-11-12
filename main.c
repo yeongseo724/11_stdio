73de8d6d426b058c94b94ce86f239e05c2e7184d
yeongseo724 <70522995+yeongseo724@users.noreply.github.com>
2020-11-12 15:36:50 +0900

    11 coding(2)

--- main.c
+++ main.c
@@ -5,23 +5,16 @@
 
 int main(int argc, char *argv[]) {
 	
-	char *pc;
-	int *pi;
-	double *pd;
+	int i=10;
+	int *ptr=&i;
 	
-	pc = (char*)10000;
-	pi = (int*)10000;
-	pd = (double*)10000;
+	printf("i = %d, pi : %p\n", i, ptr);
 	
-	printf("before : pc = %d, pi = %d, pd = %d\n",
-			pc, pi, pd);
-			
-	pc++;
-	pi++;
-	pd++;
+	(*ptr)++;
+	printf("i = %d, pi : %p\n", i, ptr);
 	
-	printf("after : pc = %d, pi = %d, pd = %d\n",
-			pc, pi,pd);
+	*ptr++;
+	printf("i = %d, pi : %p\n", i, ptr);
 	
 	return 0;
 }
